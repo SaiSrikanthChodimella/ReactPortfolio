572c6d958e05edb7738abfa1187b3ff138ecc27a
SaiSrikanthChodimella <srikanth.chodimella@gmail.com>
2025-02-02 17:56:35 +0100

    Clean Coding App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import MainLayout from "./Components/MainLayout.jsx";
-
 import Footer from "./Components/Footer.jsx";
 import Timeline from "./Components/Timeline.jsx";
 import Navbar from "./Components/Navbar.jsx";
@@ -12,6 +11,17 @@ import WorkExperience from "./Pages/WorkExperience.jsx";
 import Skills from "./Pages/Skills.jsx";
 import Certifications from "./Pages/Certifications.jsx";
 import Education from "./Pages/Education.jsx";
+
+const HomePage = () => (
+  <>
+    <AboutMe />
+    <WorkExperience />
+    <Skills />
+    <Certifications />
+    <Projects />
+    <Education />
+  </>
+);
 
 const App = () => {
   return (
@@ -26,19 +36,7 @@ const App = () => {
             <Route path="/certifications" element={<Certifications />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/education" element={<Education />} />
-            <Route
-              path="/"
-              element={
-                <>
-                  <AboutMe />
-                  <WorkExperience />
-                  <Skills />
-                  <Certifications />
-                  <Projects />
-                  <Education />
-                </>
-              }
-            />
+            <Route path="/" element={<HomePage />} />
           </Routes>
         </div>
         <Footer />
