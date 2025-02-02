95916410a385f9fb6de7076f08f6d3684cb90496
SaiSrikanthChodimella <srikanth.chodimella@gmail.com>
2025-02-02 17:58:07 +0100

    Clean Coding Education

--- src/Pages/Education.jsx
+++ src/Pages/Education.jsx
@@ -1,115 +1,87 @@
 import FAU from "../assets/FAULogo.png";
 import GITAM from "../assets/GitamLogo.jpg";
 
+const universities = [
+  {
+    name: "FAU University",
+    duration: "Sept 2021 - Present",
+    location: "Erlangen, Germany",
+    degree: "Master's in Artificial Intelligence",
+    description:
+      "In my Master's program at FAU University, I am currently focusing on advanced topics such as Human-Computer Interaction, AI Applications, and Mathematics. I am also incorporating AI tools to improve my productivity and efficiency. Currently, I am working on a project to build a SPAM Filter system, applying machine learning techniques to solve real-world problems.",
+    image: FAU,
+    link: "https://www.fau.de/",
+  },
+  {
+    name: "GITAM University",
+    duration: "Jul 2014 - May 2018",
+    location: "Visakhapatnam, India",
+    degree: "Bachelor's in Electronics and Communications Engineering",
+    description:
+      "During my Bachelor's program, I gained hands-on experience with C++, RDBMS, Data Structures and Algorithms, and AutoCAD. I also completed two internships, where I worked on projects involving RADARs and Patch Antennas, further enhancing my practical skills and technical knowledge.",
+    image: GITAM,
+    link: "https://www.gitam.edu/",
+  },
+];
+
+function UniversityCard({
+  name,
+  duration,
+  location,
+  degree,
+  description,
+  image,
+  link,
+}) {
+  return (
+    <div className="bg-white rounded-lg shadow-lg hover:shadow-xl transition-transform hover:scale-105 mb-8 bento-box">
+      <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 py-8 bento-box">
+        <div className="flex flex-col lg:flex-row items-center gap-8 bento-box">
+          <div className="w-full lg:w-1/2 bento-box">
+            <img
+              src={image}
+              alt={name}
+              className="w-full max-w-md rounded-lg"
+            />
+          </div>
+          <div className="w-full lg:w-1/2 text-center lg:text-left">
+            <h2 className="text-2xl md:text-3xl font-semibold text-gray-800 mb-4">
+              {name}
+            </h2>
+            <p className="text-gray-600 mb-4">
+              <strong>{duration}</strong>
+              <br />
+              {location}
+              <br />
+              <strong>{degree}</strong>
+            </p>
+            <p className="text-gray-600 mb-4">{description}</p>
+            <div className="mt-6">
+              <a
+                href={link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-block bg-sky-900 text-white px-6 py-2 rounded-md shadow-md hover:scale-105 transition-transform"
+              >
+                Visit {name}
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 function Education() {
   return (
     <div className="bg-gradient-to-r from-slate-900 to-gray-900 p-6 md:p-8 text-white rounded-xl shadow-lg transition-transform hover:scale-102 hover:shadow-2xl bento-box">
-      {/* Section Title */}
       <h2 className="text-3xl md:text-4xl text-center font-bold mb-8">
         Education
       </h2>
-
-      {/* FAU University Card */}
-      <div className="bg-white rounded-lg shadow-lg hover:shadow-xl transition-transform hover:scale-105 mb-8 bento-box">
-        <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 py-8 bento-box">
-          <div className="flex flex-col lg:flex-row items-center gap-8 bento-box">
-            {/* Image */}
-            <div className="w-full lg:w-1/2 bento-box">
-              <img
-                src={FAU}
-                alt="FAU University"
-                className="w-full max-w-md rounded-lg"
-              />
-            </div>
-
-            {/* Text Content */}
-            <div className="w-full lg:w-1/2 text-center lg:text-left">
-              <h2 className="text-2xl md:text-3xl font-semibold text-gray-800 mb-4">
-                FAU University
-              </h2>
-              <p className="text-gray-600 mb-4">
-                <strong>Sept 2021 - Present</strong>
-                <br />
-                Erlangen, Germany
-                <br />
-                <strong>Master's in Artificial Intelligence</strong>
-              </p>
-              <p className="text-gray-600 mb-4">
-                In my Master's program at FAU University, I am currently
-                focusing on advanced topics such as{" "}
-                <strong>Human-Computer Interaction</strong>,{" "}
-                <strong>AI Applications</strong>, and{" "}
-                <strong>Mathematics</strong>. I am also incorporating AI tools
-                to improve my productivity and efficiency. Currently, I am
-                working on a project to build a <strong>SPAM Filter</strong>{" "}
-                system, applying machine learning techniques to solve real-world
-                problems.
-              </p>
-              <div className="mt-6">
-                <a
-                  href="https://www.fau.de/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="inline-block bg-sky-900 text-white px-6 py-2 rounded-md shadow-md hover:scale-105 transition-transform"
-                >
-                  Visit FAU University
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* GITAM University Card */}
-      <div className="bg-white rounded-lg shadow-lg hover:shadow-xl transition-transform hover:scale-105 bento-box">
-        <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 py-8 bento-box">
-          <div className="flex flex-col lg:flex-row items-center gap-8 bento-box">
-            {/* Text Content */}
-            <div className="w-full lg:w-1/2 text-center lg:text-left bento-box">
-              <h2 className="text-2xl md:text-3xl font-semibold text-gray-800 mb-4">
-                GITAM University
-              </h2>
-              <p className="text-gray-600 mb-4">
-                <strong>Jul 2014 - May 2018</strong>
-                <br />
-                Visakhapatnam, India
-                <br />
-                <strong>
-                  Bachelor's in Electronics and Communications Engineering
-                </strong>
-              </p>
-              <p className="text-gray-600 mb-4">
-                During my Bachelor's program, I gained hands-on experience with
-                <strong> C++</strong>, <strong>RDBMS</strong>,{" "}
-                <strong>Data Structures and Algorithms</strong>, and{" "}
-                <strong>AutoCAD</strong>. I also completed two internships,
-                where I worked on projects involving <strong>RADARs</strong> and{" "}
-                <strong>Patch Antennas</strong>, further enhancing my practical
-                skills and technical knowledge.
-              </p>
-              <div className="mt-6">
-                <a
-                  href="https://www.gitam.edu/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="inline-block bg-sky-900 text-white px-6 py-2 rounded-md shadow-md hover:scale-105 transition-transform"
-                >
-                  Visit GITAM University
-                </a>
-              </div>
-            </div>
-
-            {/* Image */}
-            <div className="w-full lg:w-1/2 bento-box">
-              <img
-                src={GITAM}
-                alt="GITAM University"
-                className="w-full max-w-md rounded-lg"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
+      {universities.map((uni, index) => (
+        <UniversityCard key={index} {...uni} />
+      ))}
     </div>
   );
 }
